e67e80b12c6abce48959083f744a26307f07a5d5
mrityu2802 <mmrityunjay727@gmail.com>
2025-01-19 00:58:55 +0530

    ui for settings page and and logic for theme

--- fe/tailwind.config.js
+++ fe/tailwind.config.js
@@ -1,4 +1,5 @@
 import daisyui from "daisyui";
+import { THEMES } from "./src/constants";
 /** @type {import('tailwindcss').Config} */
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
@@ -6,4 +7,7 @@ export default {
     extend: {},
   },
   plugins: [daisyui],
+  daisyui: {
+    themes: THEMES,
+  },
 };
